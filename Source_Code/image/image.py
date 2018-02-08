a9819bd91c1133fe7a4a7a0e4cc20fa22ef9c474
Adam Paquette <acp263@nau.edu>
2018-02-07 21:55:17 -0700

    Updated control and image to follow current design pattern.

--- Source_Code/image/image.py
+++ Source_Code/image/image.py
@@ -1,4 +1,6 @@
 import cv2
+import math
+import numpy as np
 from utils import utils
 
 class Image:
@@ -12,7 +14,7 @@ class Image:
         return self.image_data[index]
 
     def normalize_image(self, newMin = 0, newMax = 1):
-        self.image_data['norm_image_data'] = utils.normalize(self.image_data['orig_image_data'], 0, 1)
+        self.image_data['norm_image_data'] = utils.normalize(self.image_data['orig_image_data'], newMin, newMax)
 
     def xy_extent(self):
         return (len(self.image_data['orig_image_data']),
@@ -21,26 +23,72 @@ class Image:
     def convert(self, from_image, to_image, conversion_type):
          self.image_data[to_image] = cv2.cvtColor(self.image_data[from_image], conversion_type)
 
-    def generate_mask_image(self, buffer, path, name):
-        y_val, x_val = self.xy_extent()
-        point_values = self.mask_df.point_value
+    def subtract(self, sub_image, data, varThresh = .055, varInit = .3,
+                                        varMin = 0, varMax = 1, useNorm = True):
+        if useNorm:
+            try:
+                img1 = self['norm_image_data']
+                img2 = sub_image['norm_image_data']
+            except KeyError:
+                raise KeyError('Images do not have their associated normalized images.')
+        else:
+            img1 = self[data]
+            img2 = sub_image[data]
 
-        mean_image = []
-        iterator = 0
+        fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows = False)
+        fgbg.setVarThreshold(varThresh)
+        fgbg.setVarInit(varInit)
+        fgbg.setVarMax(varMax)
+        fgbg.setVarMin(varMin)
+        fgbg.apply(img2)
+        img_mask = fgbg.apply(img1)
+        self.image_data['sub_mask'] = img_mask
+        sub_image.image_data['sub_mask']  = img_mask
 
-        for i in range(0, x_val, buffer):
-            mean_sample = []
-            for j in range(0, y_val, buffer):
-                mean_sample.append(map_percentage(point_values[iterator]))
-                iterator += 1
-            mean_image.append(np.array(mean_sample))
+    def analyze_mask_pixel(self, mask, buffer, step):
+        means = []
+        x, y = self.xy_extent()
+        for i in range(0, x, step):
+            for j in range(0, y, step):
+                crop = mask[i:(i + buffer), j:(j + buffer)]
+                means.append(np.mean(crop))
 
-        mask_image = os.path.join(path, name + '_test.png')
-        cv2.imwrite(mask_image, np.array(mean_image))
-        dist_image = cv2.imread(mask_image, 1)
-        dist_image = cv2.cvtColor(dist_image, cv2.COLOR_BGR2RGB)
-        dsize = self.xy_extent() + buffer
-        dsize = (dsize[0], dsize[1])
-        dist_image = cv2.resize(dist_image, dsize, fx=0, fy=0, interpolation = cv2.INTER_CUBIC)
-        cv2.imwrite(mask_image, dist_image)
-        return Image(path, name + '_test.png')
+        point_values = (means / max(means)) * 100
+        analyzed_image = point_values.reshape(math.ceil(x/step), math.ceil(y/step))
+        self.image_data['analyzed_image'] = analyzed_image
+
+    def analyze_mask_block(self, mask, buffer, step):
+        line = []
+        analyzed_image = []
+        x, y = self.xy_extent()
+        for i in range(0, x, step):
+            for j in range(0, y, step):
+                crop = mask[i:(i + buffer), j:(j + buffer)]
+                mean = np.mean(crop)
+                if mean > 255 * .7:
+                    mean = 255 * .99
+                elif mean > 255 * .3:
+                    mean = 255 * .5
+                else:
+                    mean = 255 * .01
+                area = np.full((buffer, buffer), mean)
+                if len(line) == 0:
+                     line = area
+                else:
+                    line = np.concatenate((line, area), axis = 1)
+            if len(analyzed_image) == 0:
+                analyzed_image = line
+            else:
+                full_image = np.concatenate((analyzed_image, line), axis = 0)
+            line = []
+        self.image_data['analyzed_image'] = analyzed_image
+
+    def color_segment(self, rock_type):
+        boundaries = utils.map_dust_colors(rock_type)
+
+        for (lower, upper) in boundaries:
+            lower = np.array(lower, dtype = "uint8")
+            upper = np.array(upper, dtype = "uint8")
+            mask = cv2.inRange(self['lab_image_data'], lower, upper)
+
+        self.image_data['color_mask'] = np.invert(mask)
