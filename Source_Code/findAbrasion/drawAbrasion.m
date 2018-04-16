e28cea5a9a4561ce9bf5c2deede3e90a620d4fcc
Beckcjb <cjb456@nau.edu>
2018-04-16 16:21:09 -0700

    Update drawAbrasion.m
    changed this to take in values and store an image

--- Source_Code/findAbrasion/drawAbrasion.m
+++ Source_Code/findAbrasion/drawAbrasion.m
@@ -1,7 +1,11 @@
-function abrasion = drawAbrasion(img_file)
+function abrasion = drawAbrasion(img_file, xcor, ycor, radius)
 % default values: xcor = 1110, ycor = 1135, radius = 320
 % Move down: increase c_col; Move up: decrease c_col
 % Move left: decrease c_row; Move right: increase c_row
+
+xcor = xcor;
+ycor = ycor;
+radius = radius;
 
 % Default values for the circle
  xcor = 1110;
@@ -12,4 +16,5 @@ function abrasion = drawAbrasion(img_file)
 img = imread(img_file);
 ci = [xcor ycor radius]; % center and radius of circle ([c_row, c_col, r])
 abrasion = insertShape(img, 'Circle', ci, 'LineWidth', 10);
+imwrite(uint8(abrasion) 'myImage.jpeg');
 end
