c7beda9c0ab16669dfc89fc6cda87df35d8488e1
Alex-Nelson <ann72@nau.edu>
2018-04-27 00:37:37 -0700

    Add files via upload
    Uses both the light and dark detection masks

---
+++ Source_Code/color_seg_matlab/combineRockEMask.m
@@ -0,0 +1,14 @@
+function [fusedMask, fusedMaskedRGBImage] = combineRockEMask(RGB)
+
+% Get the light mask of the image
+[lightBW, lightMaskedImage] = dustELight(RGB);
+
+% Get the dark mask of the image
+[darkBW, darkMaskedImage] = rockEdustDark(RGB);
+
+% If blend doens't work switch it back to 'diff'
+fusedMask = imfuse(lightBW, darkBW, 'blend');
+
+fusedMaskedRGBImage = imfuse(lightMaskedImage, darkMaskedImage, 'blend');
+
+end
--- Source_Code/color_seg_matlab/rockEMatlabColorSegment.m
+++ Source_Code/color_seg_matlab/rockEMatlabColorSegment.m
@@ -2,6 +2,8 @@ function [BW, maskedRGBImage] = matlabColorSeg(Name)
 
 image = imread(Name);
 
-[BW, maskedRGBImage] = createMask2(image);
+% [BW, maskedRGBImage] = createMask2(image);
+
+[BW, maskedRGBImage] = combineRockEMask(image);
 
 end
